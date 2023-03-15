b7947c0ad06ff6113bbaaf4f13be6de3079968f5
Zsanett Tamás <zsanett.tamas87@gmail.com>
2023-03-15 12:09:37 +0100

    server and build fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,56 @@
 // webpack.mix.js
 
-let mix = require('laravel-mix');
+const mix = require("laravel-mix");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+require("laravel-mix-ejs");
 
-mix.js('src/js/main.js', 'dist/js').setPublicPath('dist/js');
+mix.webpackConfig({
+  stats: {
+      children: true,
+  },
+});
+
+if (!mix.inProduction()) {
+  mix
+    // sets public path for manifest file
+    .setPublicPath("dist/")
+
+    // bundles js: (from, to)
+    .js("src/js/main.js", "dist/js")
+
+    // compiles sass and add css3 prefixes: (from, to)
+    .postCss("src/css/main.css", "dist/css", [
+      require("postcss-import"),
+      require("tailwindcss"),
+    ]);
+
+    // compiles ejs templates: (from, to, contents, options)
+    mix.ejs("src/index.ejs", "dist", {}, { base: "src" });
+
+    // serves assets and sync with browser sync
+    mix.browserSync({ server: 'dist', proxy: null })
+
+} else {
+  mix
+    // sets public path for manifest file
+    .setPublicPath("dist/")
+
+    // bundles js: (from, to)
+    .js("src/js/main.js", "dist/js")
+
+    // compiles sass and add css3 prefixes: (from, to)
+    .postCss("src/css/main.css", "dist/css", [
+      require("postcss-import"),
+      require("tailwindcss"),
+    ]);
+
+  // compiles ejs templates: (from, to, contents, options)
+  mix
+    .ejs("src/index.ejs", "dist", {}, { base: "src" })
+
+    // cleans dist directory and compresses assets
+    .webpackConfig({
+      plugins: [new CleanWebpackPlugin(), new CompressionPlugin()],
+    });
+}
